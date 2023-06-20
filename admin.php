aa8ecce2fff23cc75df8eabef48f1edff2d3aaa3
EnzoPlanes <croutonozne@gmail.com>
2023-06-20 20:57:00 +0200

    modif bugs form.php et admin.php

--- admin.php
+++ admin.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 
-// Vérifier si l'utilisateur est administrateur
 if (isset($_SESSION['admin']) && $_SESSION['admin'] === true) {
-    // L'utilisateur est administrateur, permettre l'accès à la page
-    // Placez ici le contenu de la page accessible uniquement aux administrateurs
+
 } else {
-    // L'utilisateur n'est pas administrateur, rediriger vers une autre page ou afficher un message d'erreur
-    header("Location: index.php"); // Redirection vers une page d'accès non autorisé
-    exit(); // Arrêter l'exécution du script
+    header("Location: index.php");
+    exit();
 }
 ?>
 
