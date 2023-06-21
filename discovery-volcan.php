7047c502405c010b4295f6b8fcf45fe53f5e2948
Sinayr7 <yanis.roche545@gmail.com>
2023-06-21 16:36:46 +0200

    ajout discovery

---
+++ discovery-volcan.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="style/style-discovery.css">
+       
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <style>
+    
+    @font-face {
+      font-family: 'SphereFez';
+      src: url('assets/font/SphereFez-Yz5g4.otf') format('opentype');
+    }
+
+    body {
+      font-family: 'SphereFez', sans-serif;
+    }
+
+    </style> 
+
+<section class="flex-container background">
+    <div class="titre">LES ÉRUPTIONS VOLCANIQUES</div>
+    <br><br>
+    <img src="assets/img/volcan.jpg" class="image-volcan">
+
+
+    <!--Origines et causes des éruptions volcaniques-->
+    <div class="def">
+    <p class="container">Spécificité du risque volcanique :</p>
+
+Les éruptions volcaniques sont des événements géologiques au cours desquels du magma, des gaz et des matériaux solides sont expulsés depuis un volcan vers la surface de la Terre. Elles sont causées par la pression du magma qui dépasse la résistance des roches volcaniques, permettant ainsi au magma de s'échapper. Les éruptions peuvent être soit effusives, avec un écoulement de lave fluide, soit explosives, avec une libération soudaine de gaz et de matériaux solides.<br><br>
+
+Les éruptions volcaniques sont des phénomènes très singuliers : alors qu’un séisme ne dure que quelques minutes, un cyclone quelques jours, certaines éruptions volcaniques peuvent durer des années et impacter de manière durable un territoire. Les volcans sont en outre capables de produire des aléas très différents et susceptibles de changer brusquement en fonction du type d'édifice et/ou d'éruptions.<br><br>
+
+Les périodes de repos plus ou moins longues entre deux éruptions (parfois de plusieurs siècles) empêchent parfois le développement d’une culture du risque adéquate.
+
+Le risque volcanique est ainsi propre à un édifice donné, et varie dans le temps et dans l’espace : chaque volcan est donc spécifique.<br><br>
+
+Les volcans se forment lorsque du magma (un mélange de roches en fusion la lave, de gaz et d’éléments solides) est émis à la surface de la Terre. On parle alors d’éruption volcanique.
+
+La plupart des volcans forment des reliefs généralement coniques, plus ou moins pentus, qui se construisent par accumulation successive de lave et/ou de matériel bréchique autour du point de sortie principal.
+
+Les volcans peuvent être terrestres ou sous-marins et leur répartition à la surface du globe n’est pas aléatoire : elle est régie par le principe général de la tectonique des plaques et des points chauds.</div>
+
+
+<!--Spécificité du risque volcanique-->
+<div class="def">
+<p class="container">Spécificité du risque volcanique :</p>
+
+  Les volcans sont des objets géologiques aux comportements hétérogènes : ils présentent tous des spécificités, qui font du risque volcanique un risque naturel à part.<br><br>
+
+  Par exemple :<br>
+
+
+  <ul>
+    <li>Certains volcans n’entreront pas en éruption à l’échelle d’une vie humaine tandis que d’autres peuvent connaître plusieurs éruptions par an.</li>
+
+    <li>Certaines éruptions peuvent être brèves (quelques jours à semaines) tandis que d’autres peuvent durer des décennies.</li>
+
+    <li>Au cours d’une même éruption, certains aléas ne sont une menace qu’à proximité immédiate du volcan, tandis que d’autres peuvent avoir des répercussions à l’échelle d’un pays entier (voire à l’échelle d’un continent).</li>
+
+    <li>Au cours d’une même éruption, différents aléas peuvent se produire (voire se succéder) à des intensités qui peuvent subitement varier de manière importante.</li>
+
+  </ul>
+
+  Le risque volcanique est ainsi spécifique à un volcan donné, et il varie dans le temps et dans l’espace.
+</div>
+
+<!--Éffets et conséquences-->
+<div class="def">
+<p class="container">Spécificité du risque volcanique :</p>
+Les éruptions volcaniques peuvent avoir des impacts (et donc produire des dégâts) très différents en fonction du type de volcan concerné, de son comportement, de son contexte géographique, et des aléas volcaniques susceptibles de survenir.<br><br>
+
+Les volcans offrent néanmoins des ressources importantes à nos sociétés (ex : géothermie, sols particulièrement fertiles, ressources minérales, tourisme, etc.), ce qui explique la présence de nombreux enjeux à proximité des édifices volcaniques, et ce malgré les risques.</div>
+
+
+<br><br>
+<div class="image-container">
+    <img src="assets/img/roche-volcanique.jpg" class="images-roche">
+    <img src="assets/img/souffre.jpg" class="images-souffre">
+</div>
+
+
+
+<!--Statistiques-->
+<div class="def">
+<p class="container">Statistiques :</p>
+Les éruptions volcaniques peuvent avoir des impacts (et donc produire des dégâts) très différents en fonction du type de volcan concerné, de son comportement, de son contexte géographique, et des aléas volcaniques susceptibles de survenir.<br><br>
+
+Les volcans offrent néanmoins des ressources importantes à nos sociétés (ex : géothermie, sols particulièrement fertiles, ressources minérales, tourisme, etc.), ce qui explique la présence de nombreux enjeux à proximité des édifices volcaniques, et ce malgré les risques.</div>
+
+
+
+<style>
+.container {
+  color: #F2F6F9;
+  font-family: 'SphereFez', sans-serif;
+  font-size: 150%;
+  font-weight: 700;
+  padding-top: 20px;
+  padding-bottom: 10px;
+  padding-right: 6rem;
+  margin-right: 0px;
+  text-align: justify;
+}
+</style>  
+
+
+
+
+</section>
--- discovery.php
+++ discovery.php
@@ -20,8 +20,13 @@
 
         @font-face {
         font-family: 'SphereFez';
-        src: url('assets/font/SphereFez-Yz5g4.otf') format('opentype');
+        /*src: url('assets/font/SphereFez-Yz5g4.otf') format('opentype');*/
         }
+
+        .parallax-one {
+    background: url('assets/img/fondparallax.jpg') fixed 50%;
+    background-size: cover;
+    } /*parallax section 1*/ 
 
     body {
         font-family: 'SphereFez', sans-serif;
@@ -37,16 +42,74 @@
 
 <section>
 
-<button>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path></svg>
-  <span>Launch</span>
+<div class='btn-container'>
+
+<button class="btn" onclick="loadContent('discovery-volcan.php')">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path fill="none" d="M0 0h24v24H0z"></path>
+    <path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path>
+  </svg>
+  <span>Éruption volcanique</span>
 </button>
 
+<button class="btn" onclick="loadContent('earth.php')">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path fill="none" d="M0 0h24v24H0z"></path>
+    <path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path>
+  </svg>
+  <span>Tsunami</span>
+</button>
+
+<button class="btn" onclick="loadContent(3)">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path fill="none" d="M0 0h24v24H0z"></path>
+    <path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path>
+  </svg>
+  <span>Tornade</span>
+</button>
+
+<button class="btn" onclick="loadContent(4)">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path fill="none" d="M0 0h24v24H0z"></path>
+    <path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path>
+  </svg>
+  <span class='result'>Météore</span>
+</button>
+
+<button class="btn" onclick="loadContent(5)">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path fill="none" d="M0 0h24v24H0z"></path>
+    <path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path>
+  </svg>
+  <span>Tremblement de terre</span>
+</button>
+
+      </div>
+      <div id="content">
+  </div>
+
+  <script>
+    function loadContent(url) {
+      var contentDiv = document.getElementById('content');
+      var xhr = new XMLHttpRequest();
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          contentDiv.innerHTML = xhr.responseText;
+        }
+      };
+
+      xhr.open('GET', url, true);
+      xhr.send();
+    }
+  </script>
+
 <style>
-    button {
+
+  .btn {
     display: flex;
     align-items: center;
-    font-family: inherit;
+    font-family: 'Poppins', sans-serif;
     font-weight: 500;
     font-size: 17px;
     padding: 0.8em 1.3em 0.8em 0.9em;
@@ -56,68 +119,62 @@
     border: none;
     letter-spacing: 0.05em;
     border-radius: 16px;
+    margin-top: 10px;
+    margin-bottom: 30px;
   }
-  
-  button svg {
+
+  .btn svg {
     margin-right: 3px;
     transform: rotate(30deg);
     transition: transform 0.5s cubic-bezier(0.76, 0, 0.24, 1);
   }
-  
-  button span {
+
+  .btn span {
     transition: transform 0.5s cubic-bezier(0.76, 0, 0.24, 1);
   }
-  
-  button:hover svg {
+
+  .btn:hover svg {
     transform: translateX(5px) rotate(90deg);
   }
-  
-  button:hover span {
+
+  .btn:hover span {
     transform: translateX(7px);
   }
 
+  .btn-container {
+        display: flex;
+        justify-content: center;
+        gap: 10px;
+        margin-top: 20px;
+  }
 
+</style>
 
-
-  </style>
-
-
-<button class="btn" onclick="changeContent(1)">Bouton 1</button>
-  <button class="btn" onclick="changeContent(2)">Bouton 2</button>
-  <button class="btn" onclick="changeContent(3)">Bouton 3</button>
-  <button class="btn" onclick="changeContent(4)">Bouton 4</button>
-  <button class="btn" onclick="changeContent(5)">Bouton 5</button>
-
-  <div id="content">
-    <p>Contenu initial.</p>
-  </div>
-
-  <script>
-          function changeContent(buttonId) {
-            var contentDiv = document.getElementById('content');
-            switch (buttonId) {
-              case 1:
-                contentDiv.innerHTML = '<p>Contenu du bouton 1.</p>';
-                break;
-              case 2:
-                contentDiv.innerHTML = '<p>Contenu du bouton 2.</p>';
-                break;
-              case 3:
-                contentDiv.innerHTML = '<p>Contenu du bouton 3.</p>';
-                break;
-              case 4:
-                contentDiv.innerHTML = '<p>Contenu du bouton 4.</p>';
-                break;
-              case 5:
-                contentDiv.innerHTML = '<p>Contenu du bouton 5.</p>';
-                break;
-              default:
-                contentDiv.innerHTML = '<p>Contenu inconnu.</p>';
-                break;
-            }
-          }
-        </script>
-    </section>
+<script>
+  function changeContent(buttonId) {
+    var contentDiv = document.getElementById('content');
+    switch (buttonId) {
+      case 1:
+        contentDiv.innerHTML = '<p style="color:white;">contenu1.</p>';
+        break;
+      case 2:
+        contentDiv.innerHTML = '<p style="color:white;">contenu2.</p>';
+        break;
+      case 3:
+        contentDiv.innerHTML = '<p style="color:white;">contenu3.</p>';
+        break;
+      case 4:
+        contentDiv.innerHTML = '<p style="color:white;">contenu4.</p>';
+        break;
+      case 5:
+        contentDiv.innerHTML = '<p style="color:white;">contenu5.</p>';
+        break;
+      default:
+        contentDiv.innerHTML = '<p>Contenu inconnu.</p>';
+        break;
+    }
+  }
+</script>
 
 <?php 
 include_once('footer.php');
